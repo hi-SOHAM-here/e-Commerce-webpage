909e78a48f17266a3b7c9dd52c85c7090afca518
hi-SOHAM-here <sohamsantra635@gmail.com>
2024-09-16 13:39:41 +0530

    js file added

---
+++ MARK UP/product-list.js
@@ -0,0 +1,54 @@
+let product_array = [];
+function product_object_builder(image, description, rating, price){
+   let  product_objcet = {
+    prdct_image: image,
+    prdct_description: description,
+    prdct_rating: rating,
+    prdct_price: price
+   };
+//    console.log(product_objcet);
+   product_array.push(product_objcet);
+}
+
+product_object_builder(
+    `../Image Bank/laptop.jfif`,
+    `HP Laptop 15s, AMD Ryzen 3 5300U, 15.6-inch (39.6 cm)`,
+    `4`,
+    `30000`
+);
+product_object_builder(
+    `../Image Bank/laptop.jfif`,
+    `HP Laptop 15s, AMD Ryzen 3 5300U, 15.6-inch (39.6 cm)`,
+    `4`,
+    `30000`
+);
+product_object_builder(
+    `../Image Bank/laptop.jfif`,
+    `HP Laptop 15s, AMD Ryzen 3 5300U, 15.6-inch (39.6 cm)`,
+    `4`,
+    `30000`
+);
+product_object_builder(
+    `../Image Bank/laptop.jfif`,
+    `HP Laptop 15s, AMD Ryzen 3 5300U, 15.6-inch (39.6 cm)`,
+    `4`,
+    `30000`
+);
+product_object_builder(
+    `../Image Bank/laptop.jfif`,
+    `HP Laptop 15s, AMD Ryzen 3 5300U, 15.6-inch (39.6 cm)`,
+    `4`,
+    `30000`
+);
+product_object_builder(
+    `../Image Bank/laptop.jfif`,
+    `HP Laptop 15s, AMD Ryzen 3 5300U, 15.6-inch (39.6 cm)`,
+    `4`,
+    `30000`
+);
+product_object_builder(
+    `../Image Bank/laptop.jfif`,
+    `HP Laptop 15s, AMD Ryzen 3 5300U, 15.6-inch (39.6 cm)`,
+    `4`,
+    `30000`
+);
